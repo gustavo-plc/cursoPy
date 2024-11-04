3fe15b469325d67ee636894fcb8eeea65613b9c5
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-04 07:44:39 -0300

    Test commiting to main branch

--- ex002.py
+++ ex002.py
@@ -2,4 +2,4 @@ nome = input('Digite seu nome: ')
 
 print('Prazer te conhecer,', nome)
 
-print('Prazer te conhecer, {}' .format(nome)) #outra forma de fazer o print na tela
+print('Prazer te conhecer, {}' .format(nome)) #outra forma de fazer o print na tela do computador
