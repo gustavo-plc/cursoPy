a8c31da22c5cf843015f3b75c8ac55ba9e3f0488
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 15:03:31 -0300

    using .format to do the printing

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
-n1 = input('Escreva um número: ')
+n1 = input('Escreva um número: ') #o retorno da função input é sempre uma string
 n2 = input('Escreva outro número: ')
 s = n1 + n2
 
@@ -11,3 +11,5 @@ n4 = int(input('Escreva outro número: '))
 s2 = n3 + n4
 
 print('O resultado da soma é: ',s2)
+
+print('O resultado da soma é :{}' .format(s2))
