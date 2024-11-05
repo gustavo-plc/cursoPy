044354604a9063c9cc5870234ebb294f7e875aa5
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 17:23:01 -0300

    sum without requiring a third variable, in case I won´t need this variable further.

--- aula07.py
+++ aula07.py
@@ -11,3 +11,8 @@ print('Prazer em te conhecer, {:>20}!'.format(nome)) #alinhado à direita
 print('Prazer em te conhecer, {:<20}!'.format(nome)) #alinhado à esquerda
 print('Prazer em te conhecer, {:^20}!'.format(nome)) #Centralizado
 print('Prazer em te conhecer, {:-^20}!'.format(nome)) #Centralizado e preenchendo espaços com caractere
+
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+
+print('A soma vale {}'.format(n1+n2)) #sem utilização de uma terceira variável
