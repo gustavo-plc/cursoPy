c326d4e716cabfe7204de17f908c96e294b1bd6b
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 15:39:59 -0300

    using type bool to get True or False from function print

---
+++ aula06b.py
@@ -0,0 +1,7 @@
+n = float(input('Digite um valor: '))
+print(n)
+
+#trabalhando com tipo booleano
+
+s = bool(input('Digite um valor: '))
+print(s) #se tiver valor na variável, qualquer um, imprime verdadeiro. Caso não tenha, imprime falso.
