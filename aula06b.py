3a49541567bb19dbfeac4e5c2cfebde2e8fe317d
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 15:43:28 -0300

    USING METHOD isNumeric() to test if the argument can be transformed into an integer

--- aula06b.py
+++ aula06b.py
@@ -5,3 +5,6 @@ print(n)
 
 s = bool(input('Digite um valor: '))
 print(s) #se tiver valor na variável, qualquer um, imprime verdadeiro. Caso não tenha, imprime falso.
+
+t = input('Digite algo: ')
+print(t.isnumeric()) #esse metodo informa se é possível converter o que foi escrito em um número INTEIRO!
