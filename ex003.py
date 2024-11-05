ab5fabf316b550cedffb03b7f42cdb4d63c531b8
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 14:00:23 -0300

    including casting to read input as an integer

---
+++ ex003.py
@@ -0,0 +1,13 @@
+n1 = input('Escreva um número: ')
+n2 = input('Escreva outro número: ')
+s = n1 + n2
+
+print('O resultado da soma é: ',s)
+
+# corrigindo:
+
+n3 = int(input('Escreva um número: ')) #casting, para ler como inteiro)
+n4 = int(input('Escreva outro número: '))
+s2 = n3 + n4
+
+print('O resultado da soma é: ',s2)
