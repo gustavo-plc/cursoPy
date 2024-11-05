d531a65e33050c532b8fc945f0430c52329d339d
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 17:20:08 -0300

    testing different format types to apply in text

--- aula07.py
+++ aula07.py
@@ -4,3 +4,10 @@
 # *, /, //, %
 # +, -
 
+nome = input('Qual é o seu nome? ')
+print('Prazer em te conhecer, {}!'.format(nome))
+print('Prazer em te conhecer, {:20}!'.format(nome)) #em 20 espaços
+print('Prazer em te conhecer, {:>20}!'.format(nome)) #alinhado à direita
+print('Prazer em te conhecer, {:<20}!'.format(nome)) #alinhado à esquerda
+print('Prazer em te conhecer, {:^20}!'.format(nome)) #Centralizado
+print('Prazer em te conhecer, {:-^20}!'.format(nome)) #Centralizado e preenchendo espaços com caractere
