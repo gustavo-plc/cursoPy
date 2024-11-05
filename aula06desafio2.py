b9222d86cdfa53de0017108dc1e3569e25da2ba4
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-05 16:20:17 -0300

    working with data information

--- aula06desafio2.py
+++ aula06desafio2.py
@@ -1,9 +1,13 @@
 n = input('Digite algo: ')
 
-print(n.isnumeric())
-print(n.isalpha())
-print(n.isalnum())
-print(n.isupper())
+print('O tipo primitivo desse dado é: {}'.format(type(n)))
+print('Só tem espaços? ',n.isspace())
+print('É um número? ', n.isnumeric())
+print('É alfabético? ', n.isalpha())
+print('É alfanumérico? ', n.isalnum())
+print('Está em maiúsculas? ', n.isupper())
+print('Está em minúsculas? ', n.islower())
+print('Está capitalizada? ', n.istitle())
 
 
 
