7957e7ef52e96f88285a44159cadcfb31eabc5e0
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-07 12:47:56 -0300

    another way to calculate hypotenuse

--- aula08desafio17.py
+++ aula08desafio17.py
@@ -4,4 +4,13 @@ c2 = int(input('Informe o outro cateto: '))
 
 h = c1**2 + c2**2
 
-print('A hipotenusa é {}'.format(sqrt(h)))
+print('A hipotenusa é {:.2f}'.format(sqrt(h)))
+
+# ou, de outra forma mais fácil
+
+from math import hypot
+
+c3 = int(input('Informe um dos catetos: '))
+c4 = int(input('Informe o outro cateto: '))
+
+print('A hipotenusa é {:.2f}'.format(hypot(c3, c4)))
