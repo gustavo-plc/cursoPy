a1d26af8fdeb4dad96c0a6b7cd973b8f391267f4
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-07 12:42:10 -0300

    code for play a mp3 file. Including bux fix

--- aula08desafio21.py
+++ aula08desafio21.py
@@ -2,5 +2,8 @@ from playsound3 import playsound
 
 playsound(r"C:\Users\gusta\OneDrive\Python\cursoPy\lalala.mp3")
 
+# o r na frente do caminho é para o interpretador não enxergar como caractere especial, o r é de raw, que significa string "raw" (bruta)
+
+
 # # or use directly on URLs
 # playsound("http://url/to/sound/file.mp3")
