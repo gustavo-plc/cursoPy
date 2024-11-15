25721f7967326d6d1c87640c7b00ebd31befd68b
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-15 17:46:40 -0300

    simple average calculation

---
+++ aula12desafio40.py
@@ -0,0 +1,12 @@
+
+n1 = int(input('escreva a primeira nota\n'))
+n2 = int(input('escreva a segunda nota\n'))
+
+media = (n1 + n2)/2
+
+if media < 5:
+    print('REPROVADO!')
+elif media >= 5 and media < 7:
+    print('RECUPRAÇÃO')
+else:
+    print('APROVADO')
