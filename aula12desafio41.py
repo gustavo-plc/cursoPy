14569ade5328b2cfa877e8b3efce32c644cb64df
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-15 18:24:50 -0300

    categories

--- aula12desafio41.py
+++ aula12desafio41.py
@@ -11,6 +11,6 @@ elif idade > 9 and idade <= 14:
 elif idade > 14 and idade <= 19:
     print('A categoria do atleta é Junior')
 elif idade > 19 and idade <= 20:
-    print('A categoria do atleta é Sênior')
+    print('A categoria do atleta é Sênior!')
 else:
     print('A categoria é master')
