95693b23b49a04d33262137f38b42a073bedfe46
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-18 07:18:53 -0300

    tiny correction

--- aula14desafio59.py
+++ aula14desafio59.py
@@ -11,16 +11,16 @@ while m != 5:
     print('5- Sair do jogo\n')
     m = int(input('Escolha uma opção: \n'))
     if m == 1:
-        print('A soma é {}\n'.format(n1+n2))
+        print('[A soma é {}]\n'.format(n1+n2))
     if m == 2:
-        print('A multiplicação é {}\n'.format(n1*n2))
+        print('[A multiplicação é {}]\n'.format(n1*n2))
     if m == 3:
         if n1 > n2:
-            print('O maior é {}\n'.format(n1))
+            print('[O maior é {}]\n'.format(n1))
         elif n2 > n1:
-            print('O maior é {}\n'.format(n2))
+            print('[O maior é {}]\n'.format(n2))
         else:
-            print('São iguais!\n')
+            print('[São iguais!]\n')
     if m == 4:
         n1 = int(input('Digite um valor: \n'))
         n2 = int(input('Digite outro valor: \n'))
