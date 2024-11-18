a19028ce832f72b0115ee7551ad8931a0d7768ac
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-11-18 08:47:15 -0300

    nested whiles

--- aula14desafio61.py
+++ aula14desafio61.py
@@ -2,7 +2,18 @@ a1 = int(input('Informe o primeiro termo da PA: \n'))
 r = int(input('Informe a razão da PA: \n'))
 c = 0
 
-while c <= 10:
+while c < 9:
     an = a1 + c*r
-    print('a{}'.format(c) + ' = ' + '{}'.format(an))
+    print('a{}'.format(c+1) + ' = ' + '{}'.format(an))
     c += 1
+
+mais = 1
+
+while mais > 0:
+    while c < c + mais:
+        an = a1 + c*r
+        print('a{}'.format(c+1) + ' = ' + '{}'.format(an))
+        c += 1
+        mais -= 1
+    mais = int(input('Digite quantos termos deseja ver a mais: \n'))
+
