5a675a6a8b7f033384e73432804f63e6e6204776
gustavo-plc <gustavoparreira@hotmail.com>
2024-11-24 16:48:41 -0300

    working with string tuples

---
+++ aula16desafio73.py
@@ -0,0 +1,7 @@
+tabela = ('Flamengo', 'Palmeiras', 'Botafogo', 'Atlético-MG', 'Grêmio', 'Fortaleza', 'Athletico-PR', 'Fluminense', 'Cruzeiro', 'Corinthians', 'Internacional', 'Red Bull Bragantino', 'São Paulo', 'Cuiabá', 'Vasco da Gama', 'Bahia', 'Santos', 'América-MG', 'Coritiba', 'Goiás')
+
+print(tabela[:5])
+print(tabela[-4:])
+print(sorted(tabela))
+print(f'O Botafogo está em {tabela.index('Botafogo')+1}º lugar!')
+
