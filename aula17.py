6b0e912615b5353e33ef3582b59815e683e10f54
gustavo-plc <gustavoparreira@hotmail.com>
2024-11-27 08:04:38 -0300

    explanation about lists: how to use them and some useful methods

---
+++ aula17.py
@@ -0,0 +1,38 @@
+#tupla é um vetor que aceita qualquer tipo de dados
+#tuplas são imutáveis! Uma vez definidos os valores de cada índice
+# do vetor, são imutáveis!
+
+#para alterar os índices de cada posição:
+#UTILIZA-SE LISTA!!!!
+
+#tuplas: ()
+#listas: []
+
+#a principal diferença é que se pode
+#alterar os valores de cada índice da lista.
+
+#listas são mutáveis, e possuem vários métodos associados!!
+
+#MÉTODOS
+#append('item')
+#insert(posição, 'item')
+# del lista[posição] - remove pela posição
+#lista.pop(posição) - remove pela posição. Sem posição? remove o último
+#lanche.remove('item') - remove pelo conteúdo
+
+#para as deleções, elimina-se o conteúdo e refaz-se os índices automaticamente
+
+#ao tentar remover um elemento que não existe: ERRO
+
+#possível criar listas por meio do range
+
+#valores = list(range(4, 11))
+# 4 5 6 7 8 9 10
+
+#ordenar valores na lista: valores.sort()
+#ordem reversa? sim: valores.sort(reverse = True)
+#len também funciona para retornar o tamanho da lista
+
+
+
+
