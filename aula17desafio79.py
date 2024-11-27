5592d64b85b23faa4ef6928f5f977137809dcf36
gustavo-plc <gustavoparreira@hotmail.com>
2024-11-27 10:57:19 -0300

    adding elements to a list and sorting them before printing on screen

---
+++ aula17desafio79.py
@@ -0,0 +1,18 @@
+lista = list()
+while True:
+    dado = int(input('Digite um valor: '))
+    if dado not in lista: #caso seja um valor inédito
+        lista.append(dado)
+        print('Valor adicionado com sucesso.')
+    else:
+        print('Valor duplicado, não adicionado! ')
+
+    #verificação de continuidade
+    cont = str(input('Deseja continuar? (S/N)')).strip().lower()
+    while cont not in {'s', 'n'}:
+        print('Dado inválido!')
+        cont = str(input('Deseja continuar? (S/N)')).strip().lower()
+    if cont == 'n':
+        print('#' * 50)
+        print(f'Você digitou os valores: {sorted(lista)}')
+        break #sai do laço infinito do while True
