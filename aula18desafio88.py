19a79e7a61da6cc8f2442149bccec806bc926c5a
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-16 11:59:03 -0300

    using random.sample module to generate sugestions to lucky numbers. Importing only the modules used with the synthax "from [library] import [module]"

--- aula18desafio88.py
+++ aula18desafio88.py
@@ -1,5 +1,5 @@
-import random
-import time
+from random import sample
+from time import sleep
 
 print('-' *30)
 print(f'{'JOGA NA MEGA SENA':^30}')
@@ -8,9 +8,9 @@ print('-' *30)
 qtd = int(input('Para quantos jogos deseja sugestão? '))
 
 for cont in range(0, qtd):
-    jogo = random.sample(range(1,61), 6) #já gera os números em uma lista
+    jogo = sample(range(1,61), 6) #já gera os números em uma lista
     jogo.sort()
     print(f'Jogo {cont + 1}: {jogo} ')
-    time.sleep(1)
+    sleep(1)
 
 print(f'{'BOA SORTE!':^30}')
