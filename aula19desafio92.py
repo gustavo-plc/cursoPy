c22c53efcbc6c53949cdb4179896affe59f0666a
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-17 17:17:26 -0300

    printing keys and their respective values of a dictionary

---
+++ aula19desafio92.py
@@ -0,0 +1,16 @@
+import datetime
+pessoa = dict()
+ano = datetime.datetime.now().year
+
+pessoa['Nome'] = str(input('Digite seu nome: ').strip())
+dn = int(input('Data de nascimento: '))
+pessoa['Idade'] = ano - dn
+pessoa['Nº da CTPS'] = input('Nº CTPS: (0 caso não tenha)')
+
+if pessoa['Nº da CTPS'] != 0:
+    pessoa['Ano de contratação'] = int(input('Digite o ano de contratação: '))
+    pessoa['Salário'] = float(input('Digite o salário: '))
+    pessoa['Ano que aposenta'] = pessoa['Ano de contratação'] + 35
+
+for k, v in pessoa.items():
+    print(f'{k} é {v}')
