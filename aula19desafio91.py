1f074d22024cff8b37e0d6355166506d69333780
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-17 16:30:12 -0300

    playing dices and sorting player's values. Using lists and dictionaries. After reversing the ranking to show the winner first

--- aula19desafio91.py
+++ aula19desafio91.py
@@ -14,7 +14,14 @@ for i in range (0,4):
 
 print('Ranking: ')
 
-ordenada = sorted(lista, key=lambda x: x['Valor'])
+ordenada = sorted(lista, key=lambda x: x['Valor'], reverse = True)
+
+#também é possível ordenar dicionários:
+#se eu não tivesse criado uma lista auxiliar, mas sim um
+#dicionário auxiliar, para ordená-lo
+#seria dessa forma:
+
+# ordenada = sorted(resultados.items(), key = itemgetter(1), reverse = True)
 
 for i in range(0,4):
     print(f'O jogador {ordenada[i]['Nome']} tirou {ordenada[i]['Valor']}')
