b936d913c520c9662b0b51891ce7572780bcc9bd
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-18 14:24:03 -0300

    including some adjustments

--- aula19desafio92.py
+++ aula19desafio92.py
@@ -1,16 +1,16 @@
-import datetime
+from datetime import datetime
 pessoa = dict()
-ano = datetime.datetime.now().year
+ano = datetime.now().year
 
 pessoa['Nome'] = str(input('Digite seu nome: ').strip())
 dn = int(input('Data de nascimento: '))
 pessoa['Idade'] = ano - dn
-pessoa['Nº da CTPS'] = input('Nº CTPS: (0 caso não tenha)')
+pessoa['Nº da CTPS'] = int(input('Nº CTPS: (0 caso não tenha) '))
 
 if pessoa['Nº da CTPS'] != 0:
     pessoa['Ano de contratação'] = int(input('Digite o ano de contratação: '))
     pessoa['Salário'] = float(input('Digite o salário: '))
-    pessoa['Ano que aposenta'] = pessoa['Ano de contratação'] + 35
+    pessoa['Ano que aposenta'] = pessoa['Ano de contratação'] - dn + 35
 
 for k, v in pessoa.items():
     print(f'{k} é {v}')
