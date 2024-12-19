7a1a00b352cca405317578fa7d877c997d85f9d4
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-19 12:46:51 -0300

    area calculation with def

---
+++ aula20desafio96.py
@@ -0,0 +1,7 @@
+def area():
+    a = float(input('Digite uma dimensão do terreno: '))
+    b = float(input('Digite a outra dimensão do terreno: '))
+    ar = a * b
+    print(f'A área de um terreno de {a} por {b} é {ar}. ')
+
+area()
