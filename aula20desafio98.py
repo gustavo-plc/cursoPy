8ec41fef9f8533d496b2b537b7c41f2bbce47b86
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-19 15:34:09 -0300

    now including a sleep function to give a counting dynamic

--- aula20desafio98.py
+++ aula20desafio98.py
@@ -1,15 +1,20 @@
+from time import sleep
+
 def contador():
     for i in range (0, 10):
         print(f'{i+1}', end = ' ')
+        sleep(0.5)
     print()
     for t in range (11, 0, -2):
         print(f'{t-1}', end = ' ')
+        sleep(0.5)
     print()
     inicio = int(input('Informe o início: '))
     fim = int(input('Informe o fim: '))
     passo = int(input('Informe o passo: '))
     for u in range (inicio, fim, passo):
         print(f'{u}', end = ' ')
+        sleep(0.5)
     print()
 
 #programa principal
