b5ef6f663c09c61beb436e30a8c5cbe19f21415b
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-28 12:09:11 -0300

    simplifying a bit more

--- utilidadesCeV/dado/__init__.py
+++ utilidadesCeV/dado/__init__.py
@@ -1,13 +1,9 @@
 def leiaDinheiro(msg):
     while True:
-        vlr = str(input(msg))
-        while vlr == '' or vlr.isspace() or vlr.isalpha():
+        vlr = str(input(msg)).replace(',', '.').strip()
+        while vlr == '' or vlr.isalpha():
             print(f'Erro! {vlr.strip()} é um preço inválido!')
-            vlr = str(input(msg))
-        if vlr.count(',') == 1:
-            vlr = vlr.replace(',', '.')
-            vlr = float(vlr)
-            break
+            vlr = str(input(msg)).replace(',', '.').strip()
         vlr = float(vlr)
         break
     return vlr
