b3a7ef08bad45a8fb3d1fc8a3ae7a2f5568aef37
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-28 09:54:14 -0300

    analysing the currency and showing some results in a formated way. Table of values. Functions

---
+++ aula22desafio110.py
@@ -0,0 +1,4 @@
+import moeda
+
+valor = float(input('Digite um preço: '))
+moeda.resumo(valor, 70, 5)
--- moeda.py
+++ moeda.py
@@ -1,3 +1,4 @@
+
 def aumentar(v, p = 0, format = False):
     """
     -> Essa função recebe um valor v e o aumenta em p porcento.
@@ -35,3 +36,19 @@ def metade(v, format = False):
 
 def moeda(v):
     return f'R$ {v}'
+
+def resumo(v, aumento = 0, reducao = 0):
+    print('-' * 50)
+    print(f'{'RESUMO DO VALOR':^50}')
+    print('-' * 50)
+    print(f'{'Preço analisado: ':<30}', end = '')
+    print(f'{f'{moeda(v)}':<20}')
+    print(f'{'Dobro do preço: ':<30}', end = '')
+    print(f'{f'{dobro(v, True)}':<20}')
+    print(f'{'Metade do preço: ':<30}', end = '')
+    print(f'{f'{metade(v, True)}':<20}')
+    print(f'{f'{aumento}% de aumento: ':<30}', end = '')
+    print(f'{f'{aumentar(v, aumento, True)}':<20}')
+    print(f'{f'{reducao}% de redução: ':<30}', end = '')
+    print(f'{f'{diminuir(v, reducao, True)}':<20}')
+    print('-' * 50)
